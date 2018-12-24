c650b54311f963c81821722d3da6ed4136cff1d4
kisasexypantera94 <green.grinya@gmail.com>
2018-12-24 05:12:33 +0300

    fix decoding, separate channel samples

--- fingerprint/fingerprint_test.go
+++ fingerprint/fingerprint_test.go
@@ -1,40 +1,48 @@
 package fingerprint
 
 import (
+	// "math"
+	"encoding/binary"
+	"bufio"
+	"bytes"
+	"fmt"
+	"github.com/hajimehoshi/go-mp3"
+	"io"
+	"os"
 	"testing"
 )
 
-func TestDecodeMpg123(t *testing.T) {
-	Decode("../resources/Metallica - Master Of Puppets.mp3")
-}
-
-// func TestDecodeGo(t *testing.T) {
-// 	f, err := os.Open("../resources/journeydontstop.mp3")
-// 	checkErr(err)
-// 	defer f.Close()
-
-// 	d, err := mp3.NewDecoder(f)
-// 	checkErr(err)
-// 	defer d.Close()
-
-// 	var rawData []float32
-// 	tmp := make([]int16, 4096)
-
-// 	// decode mp3 file and dump output
-// 	for {
-// 		buf := make([]byte, 2*len(tmp))
-// 		_, err := d.Read(buf)
-
-// 		if err != nil {
-// 			break
-// 		}
-
-// 		binary.Read(bytes.NewBuffer(buf), binary.LittleEndian, tmp)
-// 		for i := 1; i < len(tmp); i += 2 {
-// 			rawData = append(rawData, (float32)(tmp[i-1]+tmp[i])/2/math.MaxInt16)
-// 		}
-// 	}
+// func TestDecodeMpg123(t *testing.T) {
+// 	Decode("../resources/Metallica - Master Of Puppets.mp3")
 // }
+
+func TestDecodeGo(t *testing.T) {
+	filename := "../resources/modjo_lady_sample.mp3"
+	file, _ := os.Open(filename)
+	defer file.Close()
+	d, _ := mp3.NewDecoder(file)
+	defer d.Close()
+
+	var buf bytes.Buffer
+	w := bufio.NewWriter(&buf)
+	written, _ := io.Copy(w, d)
+	fmt.Println(written)
+	w.Flush()
+	r := bufio.NewReader(&buf)
+
+	tmp := make([]int16, written / 2)
+	var rawData []int16
+	binary.Read(r, binary.LittleEndian, tmp)
+	for i := 0; i < len(tmp); i += 2 {
+		x := (tmp[i] + tmp[i+1]) / 2
+		fmt.Println(x)
+		rawData = append(rawData, (x))
+	}
+
+	f, _ := os.Create(filename + "raw.raw")
+	binary.Write(f, binary.LittleEndian, rawData)
+	f.Close()
+}
 
 // func TestConvertStereoToMono(t *testing.T) {
 // 	StereoToMono("/Users/chingachgook/dev/gocode/src/github.com/kisasexypantera94/khalzam/resources/journeydontstop.mp3")
--- musiclibrary/mlib_test.go
+++ musiclibrary/mlib_test.go
@@ -1,6 +1,7 @@
 package musiclibrary
 
 import (
+	"sync"
 	// "fmt"
 	_ "github.com/lib/pq"
 	"testing"
@@ -9,16 +10,49 @@ import (
 func TestRecogniseSong(t *testing.T) {
 	musicLib, err := Open()
 	checkErr(err)
+	wg := sync.WaitGroup{}
 	defer musicLib.Close()
-	err = musicLib.DeleteSong("Modjo - Lady (Hear Me Tonight)")
-	err = musicLib.DeleteSong("Beastie Boys - Intergalactic")
-	err = musicLib.DeleteSong("Mogwai - Travel Is Dangerous")
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.DeleteSong("Modjo - Lady (Hear Me Tonight)")
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.DeleteSong("Beastie Boys - Intergalactic")
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.DeleteSong("Mogwai - Travel Is Dangerous")
+	}()
+	wg.Wait()
 
-	err = musicLib.InsertSong("../resources/Modjo - Lady (Hear Me Tonight).mp3")
-	err = musicLib.InsertSong("../resources/Beastie Boys - Intergalactic.mp3")
-	err = musicLib.InsertSong("../resources/Mogwai - Travel Is Dangerous.mp3")
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.InsertSong("../resources/Modjo - Lady (Hear Me Tonight).mp3")
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.InsertSong("../resources/Beastie Boys - Intergalactic.mp3")
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.InsertSong("../resources/Mogwai - Travel Is Dangerous.mp3")
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		musicLib.InsertSong("../resources/journeydontstop.mp3")
+	}()
+	wg.Wait()
 
 	musicLib.RecogniseSong("../resources/intergalactic_sample.mp3")
+	musicLib.RecogniseSong("../resources/intergalacticnew.mp3")
 	musicLib.RecogniseSong("../resources/modjo_lady_sample.mp3")
 	musicLib.RecogniseSong("../resources/travel_sample.mp3")
 	musicLib.RecogniseSong("../resources/travel_chorus_sample.mp3")
